a0c613a040340373c61f0793d31689828258750e
lichengyang1 <1061354368@qq.com>
2020-12-21 20:35:41 +0800

    11

---
+++ 血条/Assets/IMGUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class IMGUI : MonoBehaviour
+{
+    public float health = 0.5f;
+
+    void OnGUI()
+    {
+        //计算血条位置
+        Vector3 worldPos = new Vector3(transform.position.x, transform.position.y -0.2f, transform.position.z);
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(worldPos);
+        //生成HorizontalScrollbar
+        GUI.HorizontalScrollbar(new Rect(new Rect(screenPos.x - 100, screenPos.y, 200, 20)), 0.0f, health, 0.0f, 1.0f);
+    }
+}
